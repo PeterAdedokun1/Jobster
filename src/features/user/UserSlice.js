18d2030c6a5fd0f40ac13287a56cb26e7b06faac
peterAdedokun1 <peteradedokun436@gmail.com>
2023-06-30 19:05:38 +0100

    update

--- src/features/user/UserSlice.js
+++ src/features/user/UserSlice.js
@@ -1,6 +1,6 @@
-import { createSlice, createAsyncThunk, } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
-
+import customFetch from "../../utils/axios";
 const initialState = {
   isLoading: false,
   user: null,
@@ -9,7 +9,13 @@ const initialState = {
 export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (user, thunkAPI) => {
-    console.log(`Register User ${JSON.stringify(user)}`);
+    try {
+      const resp = await customFetch.post("/auth/register", user);
+      return resp.data;
+    } catch (error) {
+      toast.error(error.response.data.msg);
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
   }
 );
 export const loginUser = createAsyncThunk(
@@ -22,6 +28,21 @@ export const loginUser = createAsyncThunk(
 const userSlice = createSlice({
   name: "user",
   initialState,
+  extraReducers: {
+    [registerUser.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [registerUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload;
+      state.user = user;
+      toast.success(`Hello there${user.name}`);
+      state.isLoading = false;
+    },
+    [registerUser.rejected]: (state,{payload}) => {
+        state.isLoading = false;
+        toast.error(payload)
+    },
+  },
 });
 
 export default userSlice.reducer;
