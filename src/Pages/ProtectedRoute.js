a762c51d7e7ba6abd2e590074a0a46d3313888e1
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-01 10:05:22 +0100

    protected-route

---
+++ src/Pages/ProtectedRoute.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+const ProtectedRoute = ({ children }) => {
+  const { user } = useSelector((store) => store.user);
+  if (!user) {
+    return <Navigate to="/landing" />;
+  }
+  return children;
+};
+
+export default ProtectedRoute;
