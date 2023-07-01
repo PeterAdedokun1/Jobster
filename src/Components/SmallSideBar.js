6906f0d7dfef3a2325f35849a7fc1597e5191fbf
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-01 10:16:14 +0100

    update

--- src/Components/SmallSideBar.js
+++ src/Components/SmallSideBar.js
@@ -1,10 +1,31 @@
-import React from 'react'
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleSidebar } from "../features/user/UserSlice";
 
 const SmallSideBar = () => {
+  const { isSideBarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
   return (
-    <Wrapper>SmallSideBar</Wrapper>
-  )
-}
+    <Wrapper>
+      <div className={isSideBarOpen ? "sidebar-container show-sidebar" : "sidebar-container"}>
+        <div className="content">
+          <button className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default SmallSideBar
+export default SmallSideBar;
