a66732f29b91b87bed966aed4720932c107c4af8
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-03 13:17:31 +0100

    bar-chat

--- src/Components/BarChart.js
+++ src/Components/BarChart.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
-const BarChart = () => {
+const BarChartComponent = ({ data }) => {
   return (
-    <div>BarChart</div>
-  )
-}
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3 " />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey="count" fill="#3b82f6" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
 
-export default BarChart
+export default BarChartComponent;
