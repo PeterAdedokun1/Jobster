d8850d95333432a14c520f1c405c19edef866299
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-04 14:35:40 +0100

    search debounce

--- src/Components/SearchContainer.js
+++ src/Components/SearchContainer.js
@@ -2,20 +2,36 @@ import { FormRow, FormRowSelect } from ".";
 import Wrapper from "../assets/wrappers/SearchContainer";
 import { useSelector, useDispatch } from "react-redux";
 import { clearFilters, handleChange } from "../features/allJobs/allJobsSlice";
+import { useState, useMemo } from "react";
 
 const SearchContainer = () => {
-   const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
-     useSelector((store) => store.allJobs);
-   const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
-   const dispatch = useDispatch();
+  const [localSearch, setLocalSearch] = useState("");
+  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+    useSelector((store) => store.allJobs);
+  const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
+  const dispatch = useDispatch();
   const handleSearch = (e) => {
-    if (isLoading) return; 
-    dispatch(handleChange({name: e.target.name, value: e.target.value}))
-   };
-   const handleSubmit = (e) => {
+    // if (isLoading) return;
+
+    dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+  };
+  const handleSubmit = (e) => {
      e.preventDefault();
-     dispatch(clearFilters())
-   };
+     setLocalSearch("");
+     dispatch(clearFilters());
+  };
+
+  const debounce = () => {
+    let timeoutID;
+    return (e) => {
+      setLocalSearch(e.target.value);
+      clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => {
+        dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+      }, 500);
+    };
+  };
+    const optimizedDebounce = useMemo(() => debounce(), []);
   return (
     <Wrapper>
       <form className="form">
@@ -26,8 +42,8 @@ const SearchContainer = () => {
           <FormRow
             type="text"
             name="search"
-            value={search}
-            handleChange={handleSearch}
+            value={localSearch}
+            handleChange={optimizedDebounce}
           />
           {/* search by status */}
           <FormRowSelect
